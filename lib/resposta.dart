8acce414be5b0d688de92f04447b180887958689
Guilherme <47448206+matheusoreis@users.noreply.github.com>
2022-07-04 17:22:35 -0300

    Update

--- lib/resposta.dart
+++ lib/resposta.dart
@@ -8,6 +8,23 @@ class Botao extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     //return Text(texto!);
-    return ElevatedButton(onPressed: null, child: Text(texto!));
+
+    return Container(
+      width: double.infinity,
+      margin: const EdgeInsets.only(left: 20, right: 20, top: 10, bottom: 10),
+      child: ElevatedButton(
+          style: ElevatedButton.styleFrom(
+            fixedSize: const Size.fromHeight(50),
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(20),
+                    bottomRight: Radius.circular(20))),
+          ),
+          onPressed: () {},
+          child: Text(
+            texto!,
+            style: const TextStyle(color: Colors.white),
+          )),
+    );
   }
 }
